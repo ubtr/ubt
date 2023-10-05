9d34a19c9df6ee09014ec51ffaa350efb5ed3dad
Artem Mironov <jaspeen@gmail.com>
2023-10-05 10:47:05 +0300

    Fix go packages

--- go/api/proto/services/block_grpc.pb.go
+++ go/api/proto/services/block_grpc.pb.go
@@ -7,8 +7,8 @@
 package services
 
 import (
-	proto "api/proto"
 	context "context"
+	proto "github.com/ubtools/ubt/go/api/proto"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
